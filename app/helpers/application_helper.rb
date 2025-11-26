4e80e9b2ec4bc6804e9c0c034184f8dc93e954f2
Tsubasa Ota <2311140031c@ed.fuk.kindai.ac.jp>
2025-11-26 07:53:10 +0000

    背景の10分ローテーション機能追加 + レイアウト修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # 10分ごとに背景を変更する
+  def timed_background_image
+    backgrounds = ["bg1.png", "bg2.png", "bg3.png"]
+
+    minute = Time.now.min
+
+    # 10分ごとに切り替え
+    index = (minute / 10) % backgrounds.size
+
+    backgrounds[index]
+  end
 end
