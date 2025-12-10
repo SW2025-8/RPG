5bc1a4dd683398243a348eccd4da98970c823b83
2311140081b-droid <2311140081b@ed.fuk.kindai.ac.jp>
2025-12-10 06:56:52 +0000

    statuscode200返すように修正

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -1,10 +1,10 @@
 class LineController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!, raise: false
 
   def webhook
     body = request.body.read
-    puts body  # デバッグ用
-    head :ok
+    render json: { status: "ok" }, status: 200
   end
 end
 
