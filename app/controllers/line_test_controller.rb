b31f4068d95ed6ae38c71816b43fc068004890ac
2311140081b-droid <2311140081b@ed.fuk.kindai.ac.jp>
2025-12-17 07:02:17 +0000

    デバッグ

--- app/controllers/line_test_controller.rb
+++ app/controllers/line_test_controller.rb
@@ -3,6 +3,7 @@ class LineTestController < ApplicationController
     response = LineBot.broadcast
 
     render plain: "LINE response: #{response.code}"
+    render plain: "TOKEN=#{ENV['LINE_CHANNEL_ACCESS_TOKEN'].inspect}\nLINE=#{response.code}"
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -35,10 +35,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_10_083422) do
     t.integer "level", default: 1
     t.integer "exp", default: 0
     t.string "avatar_type", default: "warrior"
-    t.integer "hp", default: 100
-    t.integer "mp", default: 30
-    t.integer "str", default: 10
-    t.integer "vit", default: 10
     t.integer "battle_stage", default: 1
     t.integer "battle_position", default: 1
     t.integer "stage_exp", default: 0
